de6f49259e85dce771c78467f7f7122dbc7946b4
onmax <maximogarciamtnez@gmail.com>
2025-12-08 10:47:52 +0100

    fix: skip runtime config validation at build time

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,6 +45,7 @@ export default defineNuxtConfig({
     },
   },
   safeRuntimeConfig: {
+    validateOnBuild: false,
     $schema: v.object({
       googleApiKey: v.pipe(v.string(), v.minLength(1, 'GOOGLE_API_KEY is required')),
       openaiApiKey: v.pipe(v.string(), v.minLength(1, 'OPENAI_API_KEY is required')),
